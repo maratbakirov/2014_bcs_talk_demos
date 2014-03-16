6cd9e27d498872fe18b92250423c710f453c0234
mbakirov <mbakirov@hotmail.com>
2014-03-17 02:30:13 +1100

    code demo

---
+++ BCSCodeModel/BCSCodeModel/CodeMOdel/CodeEntityService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace BCSCodeModel.CodeMOdel
+{
+    public class CodeEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public partial class CodeEntityService
+    {
+        public static CodeEntity ReadItem(int iD)
+        {
+            return new CodeEntity()
+            {ID = iD, Name = "Name " + iD};
+        }
+
+        public static IEnumerable<CodeEntity> ReadList()
+        {
+            for (int iD = 0; iD < 100; iD++)
+            {
+                yield return new CodeEntity()
+
+                {ID = iD, Name = "Name " + iD};
+            }
+        }
+    }
+}
---
+++ BCSCodeModel/BCSCodeModel/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="b467951f-e930-4b3e-b2f4-090415ac8185" featureId="b467951f-e930-4b3e-b2f4-090415ac8185" imageUrl="" scope="Farm" solutionId="00000000-0000-0000-0000-000000000000" title="BCSCodeModel Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="1a959750-ab41-4d81-8751-064c99cb0af5" />
+  </projectItems>
+</feature>
